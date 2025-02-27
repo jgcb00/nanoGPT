ba98d972dbc12f222d465d83c4d0c2108d3692f9
Alexandre TL <alextorresleguet@icloud.com>
2025-02-27 16:15:10 +0100

    script

--- scripts/gpt2_adamw.sh
+++ scripts/gpt2_adamw.sh
@@ -13,13 +13,14 @@ torchrun --nproc_per_node=1 main.py \
     --device_batch_size 64 \
     --learning_rate 1.8e-3 \
     --num_iterations 4578 \
-    --warmup_iters 0 \
+    --warmup_iters 200 \
     --warmdown_iters 1308 \
-    --weight_decay 0 \
+    --weight_decay 0.1 \
     --sequence_length 1024 \
     --vocab_size 50304 \
     --input_bin 'data/fineweb10B/fineweb_train_*.bin' \
     --input_val_bin 'data/fineweb10B/fineweb_val_*.bin' \
     --val_loss_every 125 \
     --val_tokens 10485760 \
-    --save_every 0
+    --save_every 0 \
+    --log_wandb True
