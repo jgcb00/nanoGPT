3d05626a028423186589abff40c1565cd580bc72
Alexandre TL <alextorresleguet@icloud.com>
2025-02-27 15:55:20 +0100

    script typo

--- scripts/gpt2_adamw.sh
+++ scripts/gpt2_adamw.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #export CUDA_VISIBLE_DEVICES=0
-export CUDA_DEVICE_MAX_CONNECTIONS=1.8
+export CUDA_DEVICE_MAX_CONNECTIONS=1
 export WANDB_MODE=offline
 
 torchrun --nproc_per_node=1 main.py \
