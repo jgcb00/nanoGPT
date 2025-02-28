fe1f61ecbca378336d4b802b17a3bfdec107b814
alxndrTL <alextorresleguet@icloud.com>
2025-02-28 13:43:58 +0000

    scripts

--- scripts/dragon_spam.sh
+++ scripts/dragon_spam.sh
@@ -2,7 +2,7 @@
 
 #export CUDA_VISIBLE_DEVICES=0
 export CUDA_DEVICE_MAX_CONNECTIONS=1
-export WANDB_MODE=offline
+#export WANDB_MODE=offline
 
 # Usage: bash gpt2_adamw.sh [RUN_NAME] [NUM_GPUS=1]
 RUN_NAME=${1}
@@ -11,6 +11,8 @@ NUM_GPUS=${2:-1}
 torchrun --nproc_per_node=$NUM_GPUS main.py \
     --run_name $RUN_NAME \
     --model dragon \
+    --attn_type normal \
+    --lin_attn_type mamba2 \
     --d_model 768 \
     --n_head 12 \
     --n_layer 7 \
@@ -18,9 +20,9 @@ torchrun --nproc_per_node=$NUM_GPUS main.py \
     --optim spam \
     --batch_size 512 \
     --device_batch_size 32 \
-    --learning_rate 1.8e-3 \
+    --learning_rate 1.0e-3 \
     --num_iterations 1000 \
-    --warmup_iters 1000 \
+    --warmup_iters 0 \
     --warmdown_iters 150 \
     --weight_decay 0.1 \
     --rmsnorm False \
